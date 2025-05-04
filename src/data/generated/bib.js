07196797c54129879332dc0912f8fdaa53a1e511
FF <FF@MacBook-Pro-2.local>
2025-05-04 14:11:43 +0800

    Initial commit

---
+++ src/data/generated/bib.js
@@ -0,0 +1,113 @@
+﻿define({ entries : {
+    "DBLP:conf/atal/IchidaM024": {
+        "author": "Alexandre Yukio Ichida and Felipe Meneguzzi and Rafael C. Cardoso",
+        "booktitle": "AAMAS",
+        "cdate": "1704067200000",
+        "crossref": "conf/atal/2024",
+        "doi": "https://dl.acm.org/doi/10.5555/3635637.3662942",
+        "pages": "880--888",
+        "title": "BDI Agents in Natural Language Environments",
+        "type": "inproceedings",
+        "url": "https://dl.acm.org/doi/10.5555/3635637.3662942",
+        "year": "2024"
+    },
+    "https://doi.org/10.48550/arxiv.2201.11903": {
+        "author": "Wei,  Jason and Wang,  Xuezhi and Schuurmans,  Dale and Bosma,  Maarten and Ichter,  Brian and Xia,  Fei and Chi,  Ed and Le,  Quoc and Zhou,  Denny",
+        "copyright": "Creative Commons Attribution 4.0 International",
+        "doi": "10.48550/ARXIV.2201.11903",
+        "keywords": "Computation and Language (cs.CL),  Artificial Intelligence (cs.AI),  FOS: Computer and information sciences,  FOS: Computer and information sciences",
+        "publisher": "arXiv",
+        "title": "Chain-of-Thought Prompting Elicits Reasoning in Large Language Models",
+        "type": "misc",
+        "url": "https://arxiv.org/abs/2201.11903",
+        "year": "2022"
+    },
+    "https://doi.org/10.48550/arxiv.2210.03629": {
+        "author": "Yao,  Shunyu and Zhao,  Jeffrey and Yu,  Dian and Du,  Nan and Shafran,  Izhak and Narasimhan,  Karthik and Cao,  Yuan",
+        "copyright": "Creative Commons Attribution 4.0 International",
+        "doi": "10.48550/ARXIV.2210.03629",
+        "keywords": "Computation and Language (cs.CL),  Artificial Intelligence (cs.AI),  Machine Learning (cs.LG),  FOS: Computer and information sciences,  FOS: Computer and information sciences",
+        "publisher": "arXiv",
+        "title": "ReAct: Synergizing Reasoning and Acting in Language Models",
+        "type": "misc",
+        "url": "https://arxiv.org/abs/2210.03629",
+        "year": "2022"
+    },
+    "https://doi.org/10.48550/arxiv.2303.11366": {
+        "author": "Shinn,  Noah and Cassano,  Federico and Berman,  Edward and Gopinath,  Ashwin and Narasimhan,  Karthik and Yao,  Shunyu",
+        "copyright": "Creative Commons Attribution 4.0 International",
+        "doi": "10.48550/ARXIV.2303.11366",
+        "keywords": "Artificial Intelligence (cs.AI),  Computation and Language (cs.CL),  Machine Learning (cs.LG),  FOS: Computer and information sciences,  FOS: Computer and information sciences",
+        "publisher": "arXiv",
+        "title": "Reflexion: Language Agents with Verbal Reinforcement Learning",
+        "type": "misc",
+        "url": "https://arxiv.org/abs/2303.11366",
+        "year": "2023"
+    },
+    "https://doi.org/10.48550/arxiv.2305.10601": {
+        "author": "Yao,  Shunyu and Yu,  Dian and Zhao,  Jeffrey and Shafran,  Izhak and Griffiths,  Thomas L. and Cao,  Yuan and Narasimhan,  Karthik",
+        "copyright": "Creative Commons Attribution 4.0 International",
+        "doi": "10.48550/ARXIV.2305.10601",
+        "keywords": "Computation and Language (cs.CL),  Artificial Intelligence (cs.AI),  Machine Learning (cs.LG),  FOS: Computer and information sciences,  FOS: Computer and information sciences",
+        "publisher": "arXiv",
+        "title": "Tree of Thoughts: Deliberate Problem Solving with Large Language Models",
+        "type": "misc",
+        "url": "https://arxiv.org/abs/2305.10601",
+        "year": "2023"
+    },
+    "https://doi.org/10.48550/arxiv.2305.17390": {
+        "author": "Lin,  Bill Yuchen and Fu,  Yicheng and Yang,  Karina and Brahman,  Faeze and Huang,  Shiyu and Bhagavatula,  Chandra and Ammanabrolu,  Prithviraj and Choi,  Yejin and Ren,  Xiang",
+        "copyright": "Creative Commons Attribution 4.0 International",
+        "doi": "10.48550/ARXIV.2305.17390",
+        "keywords": "Computation and Language (cs.CL),  Artificial Intelligence (cs.AI),  Machine Learning (cs.LG),  Multiagent Systems (cs.MA),  Robotics (cs.RO),  FOS: Computer and information sciences,  FOS: Computer and information sciences",
+        "publisher": "arXiv",
+        "title": "SwiftSage: A Generative Agent with Fast and Slow Thinking for Complex Interactive Tasks",
+        "type": "misc",
+        "url": "https://arxiv.org/abs/2305.17390",
+        "year": "2023"
+    },
+    "https://doi.org/10.48550/arxiv.2307.13854": {
+        "author": "Zhou,  Shuyan and Xu,  Frank F. and Zhu,  Hao and Zhou,  Xuhui and Lo,  Robert and Sridhar,  Abishek and Cheng,  Xianyi and Ou,  Tianyue and Bisk,  Yonatan and Fried,  Daniel and Alon,  Uri and Neubig,  Graham",
+        "copyright": "arXiv.org perpetual,  non-exclusive license",
+        "doi": "10.48550/ARXIV.2307.13854",
+        "keywords": "Artificial Intelligence (cs.AI),  Computation and Language (cs.CL),  Machine Learning (cs.LG),  FOS: Computer and information sciences,  FOS: Computer and information sciences",
+        "publisher": "arXiv",
+        "title": "WebArena: A Realistic Web Environment for Building Autonomous Agents",
+        "type": "misc",
+        "url": "https://arxiv.org/abs/2307.13854",
+        "year": "2023"
+    },
+    "https://doi.org/10.48550/arxiv.2310.06770": {
+        "author": "Jimenez,  Carlos E. and Yang,  John and Wettig,  Alexander and Yao,  Shunyu and Pei,  Kexin and Press,  Ofir and Narasimhan,  Karthik",
+        "copyright": "Creative Commons Attribution 4.0 International",
+        "doi": "10.48550/ARXIV.2310.06770",
+        "keywords": "Computation and Language (cs.CL),  Artificial Intelligence (cs.AI),  Software Engineering (cs.SE),  FOS: Computer and information sciences,  FOS: Computer and information sciences",
+        "publisher": "arXiv",
+        "title": "SWE-bench: Can Language Models Resolve Real-World GitHub Issues?",
+        "type": "misc",
+        "url": "https://arxiv.org/abs/2310.06770",
+        "year": "2023"
+    },
+    "https://doi.org/10.48550/arxiv.2503.02682": {
+        "author": "Xiong,  Weimin and Song,  Yifan and Dong,  Qingxiu and Zhao,  Bingchan and Song,  Feifan and Wang,  Xun and Li,  Sujian",
+        "copyright": "Creative Commons Attribution 4.0 International",
+        "doi": "10.48550/ARXIV.2503.02682",
+        "keywords": "Computation and Language (cs.CL),  Artificial Intelligence (cs.AI),  Machine Learning (cs.LG),  FOS: Computer and information sciences,  FOS: Computer and information sciences",
+        "publisher": "arXiv",
+        "title": "MPO: Boosting LLM Agents with Meta Plan Optimization",
+        "type": "misc",
+        "url": "https://arxiv.org/abs/2503.02682",
+        "year": "2025"
+    },
+    "https://doi.org/10.48550/arxiv.2503.09572": {
+        "author": "Erdogan,  Lutfi Eren and Lee,  Nicholas and Kim,  Sehoon and Moon,  Suhong and Furuta,  Hiroki and Anumanchipalli,  Gopala and Keutzer,  Kurt and Gholami,  Amir",
+        "copyright": "arXiv.org perpetual,  non-exclusive license",
+        "doi": "10.48550/ARXIV.2503.09572",
+        "keywords": "Computation and Language (cs.CL),  FOS: Computer and information sciences,  FOS: Computer and information sciences",
+        "publisher": "arXiv",
+        "title": "Plan-and-Act: Improving Planning of Agents for Long-Horizon Tasks",
+        "type": "misc",
+        "url": "https://arxiv.org/abs/2503.09572",
+        "year": "2025"
+    }
+}});
